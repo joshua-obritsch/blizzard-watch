b6baede104057aa7531dfa3af29acffeaa4474e9
Joshua Obritsch <joshua@obritsch.com>
2022-06-04 01:32:46 +0200

    Implement basic regex

--- src/Main.hs
+++ src/Main.hs
@@ -6,11 +6,11 @@ module Main where
 
 import Prelude
 import Control.Concurrent (threadDelay)
-import Control.Monad (forever)
+import Control.Monad (forM_, forever)
 import Data.Text.Lazy (Text)
-import Language.Haskell.Exts (parseFile)
 import Language.Haskell.Interpreter
 import System.FSNotify
+import Text.Regex.PCRE ((=~))
 
 
 main :: IO ()
@@ -21,24 +21,38 @@ main =
       mgr          -- manager
       "./src"      -- directory to watch
       (const True) -- predicate
-      tmp        -- action
+      loadFile     -- action
 
     -- sleep forever (until interrupted)
     forever $ threadDelay 1000000
 
 
-tmp :: Action
-tmp = \case
-    Added    path _ _ -> do
-        file <- parseFile path
-        tmp <- runInterpreter $ do
-            setImports
-                [ "Prelude"
-                , "Blizzard.Css"
-                , "Data.Text.Lazy"
-                ]
-            interpret "rawCss [borderStyle solid]" (as :: Maybe Text)
-        print tmp
-    Modified path _ _ -> print path
-    Removed  path _ _ -> print path
+loadFile :: Action
+loadFile = \case
+    Added    path _ _ -> parseFile path
+    Modified path _ _ -> parseFile path
+    Removed  path _ _ -> pure ()
     Unknown  {}       -> pure ()
+
+
+parseFile :: String -> IO ()
+parseFile path = do
+    file <- readFile path
+    let styles = map (dropWhile (/= '[')) . concat $ (file =~ regex :: [[String]])
+    forM_ styles $ \style -> do
+        tmp <- interpretStyles style
+        print tmp
+
+
+interpretStyles :: String -> IO (Either InterpreterError (Maybe Text))
+interpretStyles styles = runInterpreter $ do
+    setImports
+        [ "Prelude"
+        , "Blizzard.Css"
+        , "Data.Text.Lazy"
+        ]
+    interpret ("rawCss " <> styles) (as :: Maybe Text)
+
+
+regex :: String
+regex = "(?>\\s)+(?>h1|h2|h3|h4|h5|h6)(?>\\s)*(?>\\[){1}[^\\]]*(?>\\]){1}"
