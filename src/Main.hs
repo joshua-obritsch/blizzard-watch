578e729d76d3ae008e996477c7b77bfd9624b4ae
Joshua Obritsch <joshua@obritsch.com>
2022-06-04 23:40:00 +0200

    Implement HashTables and output to app.css

--- src/Main.hs
+++ src/Main.hs
@@ -4,13 +4,13 @@
 module Main where
 
 
-import Prelude hiding (null)
+import Prelude hiding (null, writeFile)
 
 import Blizzard.Html (Attribute(..))
 import Clay (Css)
 import Clay.Render (compact, renderWith)
 import Control.Concurrent (threadDelay)
-import Control.Monad ((<=<), forM, forever)
+import Control.Monad ((<=<), forM_, forever)
 import Data.ByteString.Lazy (toStrict)
 import Data.Functor ((<&>))
 import Data.Hash.Murmur (murmur3)
@@ -18,46 +18,76 @@ import Data.List (intercalate)
 import Data.List.Split (splitOn)
 import Data.Maybe (catMaybes)
 import Data.Text.Lazy (Text, pack, null)
+import Data.Text.Lazy.IO (writeFile)
 import Data.Text.Lazy.Encoding (encodeUtf8)
 import Language.Haskell.Interpreter
 import System.FSNotify
 import Text.Regex.Posix ((=~), getAllTextMatches)
 
+import qualified Data.HashTable.IO as Dict
+
+
+type Dict = Dict.BasicHashTable String [(Text, Text)]
+type Hash = Dict.BasicHashTable Text Text
+
 
 main :: IO ()
 main =
-  withManager $ \mgr -> do
-    -- start a watching job (in the background)
-    watchTree
-      mgr          -- manager
-      "./src"      -- directory to watch
-      (const True) -- predicate
-      loadFile     -- action
+    withManager $ \mgr -> do
+        dict <- Dict.new :: IO Dict
 
-    -- sleep forever (until interrupted)
-    forever $ threadDelay 1000000
+        -- start a watching job (in the background)
+        watchTree
+            mgr           -- manager
+            "./src"       -- directory to watch
+            (const True)  -- predicate
+            (loadFile dict) -- action
+
+        -- sleep forever (until interrupted)
+        forever $ threadDelay 1000000
 
 
-loadFile :: Action
-loadFile = \case
-    Added    path _ _ -> parseFile path
-    Modified path _ _ -> parseFile path
+loadFile :: Dict -> Action
+loadFile dict = \case
+    Added    path _ _ -> parseFile dict path
+    Modified path _ _ -> parseFile dict path
     Removed  path _ _ -> pure ()
     Unknown  {}       -> pure ()
 
 
-parseFile :: String -> IO ()
-parseFile path
+parseFile :: Dict -> String -> IO ()
+parseFile dict path
      =  readFile path
     >>= mapM interpretProps . parseCss
-    >>= print
+    >>= updateDict dict path
      .  zipCss
      .  filter (not . null)
+
+
+updateDict :: Dict -> String -> [(Text, Text)] -> IO ()
+updateDict dict key value = do
+    hash <- Dict.new :: IO Hash
+    Dict.insert dict key value
+    Dict.mapM_ (updateHash hash) dict
+    Dict.foldM concatHash "" hash
+        >>= writeFile "app.css"
+
+
+concatHash :: Text -> (Text, Text) -> IO Text
+concatHash a (b, c) =
+    pure $ a <> b <> c
+
+
+updateHash :: Hash -> (String, [(Text, Text)]) -> IO ()
+updateHash hash (_, v) = do
+    forM_ v $ \(k, v) -> do
+        Dict.insert hash k v
 
 
 hashCss :: Text -> Text
 hashCss
     = pack
+    . ('.' :)
     . ('_' :)
     . show
     . murmur3 15739
